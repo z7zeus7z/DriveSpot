a60f71b19567c242d0081dc16d39fde0c0610784
Zaid Sabbah <zaidsabbah89@gmail.com>
2025-11-01 17:30:49 +0300

    Update AdCard component with new background image and button link

--- Client/src/Components/Home/AdCard.jsx
+++ Client/src/Components/Home/AdCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import style from '../../Style/Home.module.css'
+import { Link } from 'react-router-dom'
 const AdCard = (props) => {
   const {title,info,img}=props
   
@@ -8,7 +9,7 @@ const AdCard = (props) => {
         <div  style={{ backgroundImage: `url(${img})` }} className={style.carCard}>
                 <div className={style.cardTitle}><h3>{title}</h3></div>
                 <div className={style.cardinfo}><h5>{info}</h5></div>
-                <button className={style.rentButton}>Renatl Car</button>
+               <Link to='/cars'><button className={style.rentButton}>Renatl Car</button></Link> 
         </div>
     </>
   )
