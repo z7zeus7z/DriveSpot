724ba139150775a46c3c7a2a963b2be5e6cb3076
Zaid Sabbah <zaidsabbah89@gmail.com>
2025-11-01 16:22:58 +0300

    Fix uploads static path and add test-upload route

--- Server/server.js
+++ Server/server.js
@@ -5,12 +5,21 @@ require ('dotenv').config();
 const userRoutes = require('./Routes/userRoutes');
 const carRoutes = require('./Routes/carRoutes');
 const path = require('path');
+const fs = require('fs');
 const PORT = process.env.PORT
 const app = express();
 //MIDWARE
-app.use(cors());
+app.use(cors({
+  origin: ['https://drive-spot.vercel.app'], // your frontend domain
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
 app.use(express.json());
-
+const uploadsDir = path.join(__dirname, 'uploads/cars');
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir, { recursive: true });
+  console.log('Created uploads/cars folder automatically');
+}
 //DB
 mongoose.connect(process.env.MONGO_URL)
 .then(()=>console.log("Connected to DB"))
@@ -18,7 +27,11 @@ mongoose.connect(process.env.MONGO_URL)
 //ROUTES
 app.use('/api/users',userRoutes);
 app.use('/api/cars', carRoutes);
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(path.resolve(__dirname, 'uploads')));
+
+app.get('/test-upload', (req, res) => {
+  res.send('Uploads folder is configured!');
+});
 
 app.listen(PORT,()=>{
     console.log(` Server running on port ${PORT}`)
